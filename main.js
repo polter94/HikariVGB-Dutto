edf66e90e478bde4fca41d3320b7d132551a12eb
Marco <marcodutto1994@gmail.com>
2024-02-14 14:22:37 -0300

    se empieza a mudar lista y carrito de html a JS

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+//OBJETO 'productos'
+// TODO
+
+//CONSTRUCTOR de la lista de productos - asignar clase cont-tienda
+//1 crea nodo
+let productos = document.createElement("div");
+//2 asigna contenido
+productos.innerHTML = " <h2> Productos </h2>"
+//3 asignacion de padre a traves de contenedor
+let contProductos = document.getElementById("listaProd");
+contProductos.append(productos);
+
+//CONSTRUCTOR del carrito - asignar clase cont-carrito
+//1 crea nodo
+let carrito = document.createElement("div");
+//2 asigna contenido
+carrito.innerHTML = " <h2> Carrito </h2>"
+//3 asignacion de padre a traves de contenedor
+let contCarrito = document.getElementById("listaProd");
+contCarrito.append(carrito);
+
+
 //  FUNCION para cambiar ${precio} por 'sin stock'
+// TODO
 
 //  FUNCION para identificar el boton 'comprar' que se presiono
+// TODO
 const btnComprar = document.querySelectorAll(".btn-comprar");
-
-let boton = document.querySelectorAll(".btn-comprar");
-for (let i = 0; i < boton.length; i++) {
-    boton[i].addEventListener("click", funcion(){
-        alert("fue presionado");
-});
-}
 
 //      no se por que no funciona si se supone que debe ser una funcion anonima, pero tira "funcion" is not defined ???
 // btnComprar.forEach(boton => {
@@ -17,3 +34,11 @@ for (let i = 0; i < boton.length; i++) {
 //         console.log('Boton presionado con ID: ', prodId);
 //     };
 // });
+
+//      segundo intento, mismo reslutado, no funciona
+// let boton = document.querySelectorAll(".btn-comprar");
+// for (let i = 0; i < boton.length; i++) {
+//     boton[i].addEventListener("click", funcion(){
+//         alert("fue presionado");
+// });
+// }
