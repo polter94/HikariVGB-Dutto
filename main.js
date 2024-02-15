d459e7f1a92ae5c999734a8d0e9599e2215750b7
Marco <marcodutto1994@gmail.com>
2024-02-15 16:35:29 -0300

    se agrega constructor en JS para lista de productos

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 //OBJETO 'productos'
+let productos = [
+    {id: 1, nombre: "Anillo", precio: 3500},
+    {id: 2, nombre: "Dije anillo", precio: 2500},
+    {id: 3, nombre: "Dije cuadrado", precio: 5000},
+    {id: 4, nombre: "Dije cuadrado XL", precio: 1500},
+    {id: 5, nombre: "Dije gota", precio: 9000},
+    {id: 6, nombre: "Dije #", precio: 4000}
+]
 // TODO
 
 //CONSTRUCTOR de la lista de productos - asignar clase cont-tienda
-//1 crea nodo
-let productos = document.createElement("div");
-//2 asigna contenido
-productos.innerHTML = " <h2> Productos </h2>"
-//3 asignacion de padre a traves de contenedor
-let contProductos = document.getElementById("listaProd");
-contProductos.append(productos);
+let cards = document.getElementById("listaProd");
+
+productos.forEach((item) => {
+    let div = document.createElement("div");
+    div.className = "producto-card";
+    div.innerHTML = `
+    <img src="./img/productos/${item.id}.png" alt="" class="producto-img">
+    <p>${item.nombre}</p>
+    <p id="precio">$ ${item.precio}</p>
+    <button type="button" class="btn-comprar">Agregar al carrito</button>
+    `;
+    cards.append(div);
+});
 
 //CONSTRUCTOR del carrito - asignar clase cont-carrito
-//1 crea nodo
 let carrito = document.createElement("div");
-//2 asigna contenido
-carrito.innerHTML = " <h2> Carrito </h2>"
-//3 asignacion de padre a traves de contenedor
-let contCarrito = document.getElementById("listaProd");
+carrito.innerHTML = "<h2>carrito</h2>"
+carrito.className = "cont-carrito";
+
+let contCarrito = document.getElementById("carrito");
 contCarrito.append(carrito);
 
 
